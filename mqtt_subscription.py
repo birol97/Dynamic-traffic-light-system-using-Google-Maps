c9b0b2b5b03556df70d0f49543ac805b839653bf
birol <74475836+birol97@users.noreply.github.com>
2022-08-18 21:01:25 +0300

    Update mqtt_subscription.py
    Code updated to work with synchronization with the traffic_light code.

--- mqtt_subscription.py
+++ mqtt_subscription.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import paho.mqtt.client as mqtt
 import traffic_light
+
+from traffic_light import traffic_light_cycle
 def on_connect(client, userdata, flags, rc):
     # This will be called once the client connects
     print(f"Connected with result code {rc}")
@@ -8,10 +10,11 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("traffic_status")
 def on_message(client, userdata, msg):
     print(f"Message received [{msg.topic}]: {msg.payload}")
-    traffic_light.lightcycle()
+
+    traffic_light_cycle.receive_priority(msg.payload);
+
 client = mqtt.Client("mqtt-test") # client ID "mqtt-test"
 client.on_connect = on_connect
 client.on_message = on_message
 client.username_pw_set("birol", "123456")
 client.connect('127.0.0.1', 1883)
-client.loop_forever()  # Start networking daemon
